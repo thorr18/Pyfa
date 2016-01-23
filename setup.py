261f9c34bfbe7794417fd2b42ccd160f9ff7d5f3
Bryce <mbp@bryceEmiller.com>
2016-01-23 00:49:54 -0800

    Let's try without py2app

--- setup.py
+++ setup.py
@@ -1,27 +1,23 @@
 """
 Distribution builder for pyfa.
-
-Windows executable: python setup.py build
-Windows executable + installer: python setup.py bdist_msi
 """
 import requests.certs
 import sys
 if 'darwin' in sys.platform:
-    print 'darwin setup.py ', __name__
-    from setuptools import setup
-    APP = ['pyfa.py']
-    DATA_FILES = ['eve.db', 'README.md', 'LICENSE', 'imgs', requests.certs.where()]
-    OPTIONS = {
-        'argv_emulation': False,
-        'iconfile': 'dist_assets/mac/pyfa.icns',
-        'packages': ['eos', 'gui', 'service', 'utils']
-    }
-    setup(
-            app=APP,
-            data_files=DATA_FILES,
-            options={'py2app': OPTIONS},
-            setup_requires=['py2app'],
-    )
+    import config
+    try:
+        from setuptools import setup
+    except ImportError:
+        from distutils.core import setup
+        setup(
+                name='Pyfa',
+                version='{}'.format(config.version),
+                description='Python fitting assistant',
+                url='https://github.com/pyfa-org/Pyfa',
+                packages=['', 'eos', 'gui', 'service', 'utils'],
+                #entry_points={'gui_scripts': ['pyfa = pfya.__main__:main']},
+                resources=['imgs']
+        )
 else:
     # The modules that contain the bulk of teh source
     packages = ['eos', 'gui', 'service', 'utils']
@@ -36,65 +32,65 @@ else:
     # All the other stuff is crap that I have. VENV isn't working right for me for a few dependancies, so bleh
     excludes = ['Tkinter', 'collections.abc', 'IPython', 'PyQt4', 'PIL', 'nose', 'tornado', 'zmq', 'mysql', 'scipy']
 
-if __name__ == "__main__":
-    from cx_Freeze import setup, Executable
-    import config
+    if __name__ == "__main__":
+        from cx_Freeze import setup, Executable
+        import config
 
 
-    app_name = 'pyfa'
-    app_version = '{}'.format(config.version)
-    app_description = 'Python fitting assistant'
+        app_name = 'pyfa'
+        app_version = '{}'.format(config.version)
+        app_description = 'Python fitting assistant'
 
-    # Windows-specific options
-    build_options_winexe = {
-        'packages': packages,
-        'include_files': include_files,
-        'includes': includes,
-        'excludes': excludes,
-        'compressed': True,
-        'optimize': 2,
-        'include_msvcr': True,
-    }
+        # Windows-specific options
+        build_options_winexe = {
+            'packages': packages,
+            'include_files': include_files,
+            'includes': includes,
+            'excludes': excludes,
+            'compressed': True,
+            'optimize': 2,
+            'include_msvcr': True,
+        }
 
-    build_options_winmsi = {
-        'upgrade_code': '{E80885AC-31BA-4D9A-A04F-9E5915608A6C}',
-        'add_to_path': False,
-        'initial_target_dir': r'[ProgramFilesFolder]\{}'.format(app_name),
-    }
-
-
-    # Mac-specific options (untested)
-    build_options_macapp = {
-        'iconfile': 'dist_assets/mac/pyfa.icns',
-        'bundle_name': app_name,
-    }
-
-    build_options_macdmg = {
-        'volume_label': app_name,
-        'applications-shortcut': True,
-    }
+        build_options_winmsi = {
+            'upgrade_code': '{E80885AC-31BA-4D9A-A04F-9E5915608A6C}',
+            'add_to_path': False,
+            'initial_target_dir': r'[ProgramFilesFolder]\{}'.format(app_name),
+        }
 
 
-    # Generic executable options
-    executable_options = {
-        'script': 'pyfa.py',
-        # Following are windows-specific options, they are stored
-        # on a per-executable basis
-        'base': 'Win32GUI' if sys.platform=='win32' else None,
-        'icon': 'dist_assets/win/pyfa.ico',
-        'shortcutDir': 'DesktopFolder',
-        'shortcutName': app_name,
-    }
+        # Mac-specific options (untested)
+        build_options_macapp = {
+            'iconfile': 'dist_assets/mac/pyfa.icns',
+            'bundle_name': app_name,
+        }
 
-    setup(
-        name=app_name,
-        version=app_version,
-        description=app_description,
-        options = {
-            'build_exe': build_options_winexe,
-            'bdist_msi': build_options_winmsi,
-            'bdist_mac': build_options_macapp,
-            'bdist_dmg': build_options_macdmg,
-        },
-        executables=[Executable(**executable_options)]
-    )
+        build_options_macdmg = {
+            'volume_label': app_name,
+            'applications-shortcut': True,
+        }
+
+
+        # Generic executable options
+        executable_options = {
+            'script': 'pyfa.py',
+            # Following are windows-specific options, they are stored
+            # on a per-executable basis
+            'base': 'Win32GUI' if sys.platform=='win32' else None,
+            'icon': 'dist_assets/win/pyfa.ico',
+            'shortcutDir': 'DesktopFolder',
+            'shortcutName': app_name,
+        }
+
+        setup(
+            name=app_name,
+            version=app_version,
+            description=app_description,
+            options = {
+                'build_exe': build_options_winexe,
+                'bdist_msi': build_options_winmsi,
+                'bdist_mac': build_options_macapp,
+                'bdist_dmg': build_options_macdmg,
+            },
+            executables=[Executable(**executable_options)]
+        )
