73bb659c1092bc532de4f90561d39da41256a07c
Bryce <mbp@bryceEmiller.com>
2016-01-31 18:48:43 -0800

    trying different entry points

--- __main__.py
+++ __main__.py
@@ -1,8 +1,20 @@
+print "module main"
+print __name__
 def main():
+    print "enter main()"
     if __name__ == '__main__':
-        print "not __main__"
+        print "yes __main__"
         import pyfa
     else:
-        print "yes main"
+        print "not main"
         import pyfa
+    print "exit main()"
+
+print "half done main module"
+if __name__ == '__main__':
+    print "yes __main__ 2"
+    import pyfa
+else:
+    print "not main 2"
+    import pyfa
 print "exit main module"
--- entry.py
+++ entry.py
@@ -1,8 +1,20 @@
+print "module entry"
+print __name__
 def entry():
+    print "enter entry()"
     if __name__ == '__main__':
-        print "not __main__"
+        print "yes __main__"
         import pyfa
     else:
-        print "yes main"
+        print "not main"
         import pyfa
+    print "exit entry()"
+
+print "half done entry module"
+if __name__ == '__main__':
+    print "yes __main__ 2"
+    import pyfa
+else:
+    print "not main 2"
+    import pyfa
 print "exit entry module"
